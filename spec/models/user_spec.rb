07c9dabe052ff1aa327a4005ea2f43ea811dbb28
yogoken <yogo.k@husky.neu.edu>
2016-08-04 11:25:47 +0900

    Test user validation

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+    it "is valid with a nickname, email, password, password_confirmation" do
+      user = User.new(nickname: "abe", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      expect(user).to be_valid
+    end
+
+
+    it "is invalid without a nickname" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
+    it "is invalid with a nickname that have more than 7 characters " do
+      user = User.create(nickname: "shinbo", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      another_user = User.new(nickname: "abe", email: user.email, password: "00000000", password_confirmation: "00000000")
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+  end
+end
